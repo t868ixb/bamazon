ae91d6503008000b9c40863987058ea8c12568bb
“IBayon“ <IBayon@Enova.com>
2018-11-27 22:09:35 -0600

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # bamazon
+
+# Application description
+The bamazon command line application reads from a MySQL database and displays a list of products they can purchase. The application will prompt the user to choose a product and how many of the product they want.  If the product and quantity is available, the application will calculate the total cost of the order, will deduct the item from inventory (the database), and will finally display to the user information about the order.  If the number of items requested is not in inventory (not available), the application will alert the customer and ask them to come again later.  
+
+# Required NPM Packages
+npm i mysql
+npm i inquirer
+
+# Database requirements
+bamazon.sql contains basic SQL commands to create the database and products table.  it also example items that were added to the table
---
+++ bamazon.sql
@@ -0,0 +1,56 @@
+-- DROP DATABASE IF EXISTS bamazon;
+-- CREATE database bamazon;
+
+-- USE bamazon;
+
+-- CREATE TABLE products (
+--   item_id INT AUTO_INCREMENT NOT NULL,
+--   product_name VARCHAR(255) NOT NULL,
+--   department_name VARCHAR(255) NOT NULL,
+--   price DECIMAL(10,2) NULL,
+--   stock_quantity INT NOT NULL,
+--   PRIMARY KEY (item_id)
+-- );
+  
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("Sofa","Furniture" ,72.00, 5);
+
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("Fork","Kitchen" ,1.00, 72);
+
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("Light Bulb","Lighting" ,7.00, 27);
+
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("wipes","cleaning" ,18.00, 52);
+
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("Coffee Grinder","Kitchen" ,37.00, 7);
+
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("Water Bottle","Sports" ,5.00, 70);
+
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("Toilet","Bathrooms" ,76.00, 5);
+
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("Donald Trump Toilet Paper","Bathrooms" ,3.00, 82);
+
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("Chalk Board","Arts and Crafts",30.00, 12);
+
+-- INSERT INTO products (product_name,department_name, price, stock_quantity)
+-- VALUES ("Softball","Sports" ,1.00, 100);
+--   
+
+Select *
+From products
+
+
+
+
+
+
+
+
+
